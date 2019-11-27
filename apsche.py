9fc708c9f41df61cbe1aa0d39c8d9c1b1ee2483f
shivampip <shivam301296@gmail.com>
2019-11-27 15:23:14 +0530

    updated

--- apsche.py
+++ apsche.py
@@ -1,31 +1,41 @@
+## https://apscheduler.readthedocs.io/en/3.0/userguide.html
+
 from datetime import datetime, timedelta
 import time
 import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+
 
 scheduler = BackgroundScheduler()
 
 
-def tick():
-    scheduler.print_jobs()                     
-    print('Tick! The time is: %s' % datetime.now())
+def tick(text):
+    print(f"{datetime.now()}: RUN: {text}")
 
 
-
+def my_listener(event):
+    if(event.exception):
+        print(f'{event.job_id} The job crashed :(')
+    else:
+        print(f'{event.job_id} The job worked :)')
 
 
 if(__name__ == '__main__'):
     scheduler.start()
+    scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
     
     print("adding jobs")
     #scheduler.add_job(tick, 'interval', seconds=3)
-    j1= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 3))
-    j2= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 4))
-    j3= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 6))
-    print(j1.id) 
+    j1= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 3), kwargs= {"text": "Hello World 1"})
+    j2= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 4), kwargs= {"text": "Hello World 2"})
+    j3= scheduler.add_job(tick, "date", next_run_time= datetime.now()+ timedelta(seconds= 6), kwargs= {"text": "Hello World 3"})
+    #print(j1.id) 
     scheduler.remove_job(j1.id)
-    scheduler.remove_job(j3.id) 
+    #scheduler.remove_job(j3.id) 
+    
+    #scheduler.print_jobs()                     
     
     #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
